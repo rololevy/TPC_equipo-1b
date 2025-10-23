48480acf6f5b36ef71a91dbd77fd57d12f87aa90
Rolo <o.oaguilera@gmail.com>
2025-10-23 06:40:11 -0300

    agregados al dominio: Cliente, Proveedor y Usuario

---
+++ Equipo1b-TPC/Dominio/Proveedor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Equipo1b_TPC.Dominio
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string RazonSocial { get; set; }
+        public string CUIT { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public bool Activo { get; set; }
+
+        public Proveedor()
+        {
+            Activo = true;
+        }
+
+        public override string ToString()
+        {
+            return RazonSocial;
+        }
+    }
+}
